d3e48a2439ab5341d6869226cbf568ec8e1d3943
JamesfChen <hawks.jamesf@gmail.com>
2022-09-21 07:42:26 +0800

    init project

--- pages/index/index.js
+++ pages/index/index.js
@@ -9,7 +9,25 @@ Page({
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo'),
     canIUseGetUserProfile: false,
-    canIUseOpenData: wx.canIUse('open-data.type.userAvatarUrl') && wx.canIUse('open-data.type.userNickName') // 如需尝试获取用户信息可改为false
+    canIUseOpenData: wx.canIUse('open-data.type.userAvatarUrl') && wx.canIUse('open-data.type.userNickName'), // 如需尝试获取用户信息可改为false
+    total: 0,
+    inputDatas: [{
+      name: 'base',
+      placeholder: '年薪',
+      unit: ''
+    }, {
+      name: 'monday',
+      placeholder: '一周工作日加班时间',
+      unit: '小时'
+    }, {
+      name: 'sunday',
+      placeholder: '一周休息日加班时间',
+      unit: '天'
+    }, {
+      name: 'newYear',
+      placeholder: '一年节假日加班时间',
+      unit: '天'
+    }]
   },
   // 事件处理函数
   bindViewTap() {
@@ -43,6 +61,52 @@ Page({
     this.setData({
       userInfo: e.detail.userInfo,
       hasUserInfo: true
+    })
+  },
+
+  formSubmit(e) {
+    console.log('form发生了submit事件，携带数据为：', e.detail.value)
+  },
+
+  formReset(e) {
+    console.log('form发生了reset事件，携带数据为：', e.detail.value)
+    this.setData({
+      chosen: ''
+    })
+  },
+  jump2href(e) {
+    wx.navigateTo({
+      url: '../w/w'
+    })
+  },
+   getFields(selector) {
+    return wx.createSelectorQuery().select(selector).fields({
+      properties: ['value']
+    }).exec()
+  },
+  update(e) {
+    //1.当前的input value值
+    console.log(e,e.detail.value,e.currentTarget.id);
+    //2.计算其他input的value值
+    let inpD = this.data.inputDatas
+    let input = {}
+    for (let i = 0; i < inpD.length; i++) {
+      if(inpD[i].name === e.currentTarget.id) continue
+      let n =this.getFields('#' + inpD[i].name);
+      console.log(inpD[i].name,n)
+      // console.log('query res: ', res,'#'+inpD[i].name);
+      // input[inpD[i].name] = Number(res.value || 0);
+    }
+    let {
+      base,
+      monday,
+      sunday,
+      newYear
+    } = input
+    let money = base * (1.5 * monday / 40 + 2 * sunday / 5 + 3 * newYear / 261)
+    let round = Math.round(money * 100) / 100
+    this.setData({
+      total: round
     })
   }
 })
