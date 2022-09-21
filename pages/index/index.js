207eb19bbc685d82c06a8d0f819f147161a30c9f
JamesfChen <hawks.jamesf@gmail.com>
2022-09-22 00:21:01 +0800

    fixed

--- pages/index/index.js
+++ pages/index/index.js
@@ -12,7 +12,7 @@ Page({
         canIUseOpenData: wx.canIUse('open-data.type.userAvatarUrl') && wx.canIUse('open-data.type.userNickName'), // 如需尝试获取用户信息可改为false
         total: 0,
         value1: 0,
-        value2: 1,
+        value2: 0,
         value3: 0,
         value4: 0,
     },
